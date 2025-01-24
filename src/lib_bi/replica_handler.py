639cb8b433ea57ac38035e02c4a6a05917dc4217
betzamica <betzabeth.maestre@micasino.com>
2025-01-24 16:24:05 +0000

    replica_handler add parameters to execute_query_to_pl_df

--- src/lib_bi/replica_handler.py
+++ src/lib_bi/replica_handler.py
@@ -1,8 +1,9 @@
 import polars as pl
 import psycopg2
 
+
 class ReplicaHandler:
-    def __init__(self, database_name:str, port:int):
+    def __init__(self, database_name: str, port: int):
         """Initializes the connection to the database."""
         self.user = "micasino-bi-cloud-sql@mi-casino.iam"
         self.host = "localhost"
@@ -12,11 +13,13 @@ class ReplicaHandler:
         self.conn = psycopg2.connect(
             host=self.host,
             database=self.dbname,
-            user=self.user ,
+            user=self.user,
             port=self.port,
         )
 
-    def execute_query_to_pl_df(self, sql: str) -> pl.DataFrame:
+    def execute_query_to_pl_df(
+        self, sql: str, execute_options: dict = None
+    ) -> pl.DataFrame:
         """Executes an SQL query and returns the results as a polars df."""
-        result = pl.read_database(sql, self.conn)
+        result = pl.read_database(sql, self.conn, execute_options=execute_options)
         return result
